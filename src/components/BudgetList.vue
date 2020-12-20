487f10910231a529a756604acdfecb0aaee30c82
Andrii <kulchaka@gmail.com>
2020-12-20 16:51:38 +0100

    first budget list

---
+++ src/components/BudgetList.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="budget-list-wrap">
+        <el-card :header="header">
+           <template v-if="noEmpty">
+                <div class="list-item" v-for="(item, prop) in list" :key="prop">
+                <span class="budget-comment">{{ item.comment }}</span>
+                <span class="budget-value">{{ item.value }}</span>
+                <el-button type="danger" size="mini">Delete</el-button>
+            </div>
+           </template>
+           <el-alert v-else type="info" show-icon :closable="false" :title="emptyTitle"/>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BudgetList',
+    props: {
+        list: {
+            type: Object,
+            default: () => ({})
+        }
+    },
+    data: () => ({
+        header: 'Budget List',
+        emptyTitle: 'Empty List',
+    }),
+    computed: {
+        noEmpty() {
+            return Boolean(Object.keys(this.list).length);
+        }
+    }
+};
+</script>
+
+<style scoped>
+.budget-list-wrap {
+    max-width: 500px;
+    margin: 0 auto;
+}
+.list-item {
+    display: flex;
+    align-items: center;
+    padding: 10px 15px;
+}
+.budget-value {
+    font-weight: bold;
+    margin-left: auto;
+    margin-right: 20px;
+}
+</style>
